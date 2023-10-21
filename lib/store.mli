3a9e277a0e00473ca6cf0020c7a3729a1bb68abf
Alina <ay332@cornell.edu>
2023-10-21 12:48:56 -0400

    Comment out store implementation for now

--- lib/store.mli
+++ lib/store.mli
@@ -1,11 +1,5 @@
-module type StoreType = sig
-    type 'a t = 'a list
-    val count_products: 'a t -> int
-    val all_products: 'a t -> 'a t
-    val import_goods: 'a t -> 'a t
-    val sell_goods: 'a t -> 'a t
-    val popular_goods: 'a t -> 'a t
-    val on_sale_goods: 'a t -> 'a t
-    val in_stock_goods: 'a t -> 'a t
-    val out_of_stock_goods: 'a t
-  end
+(* module type StoreType = sig type 'a t = 'a list val count_products: 'a t ->
+   int val all_products: 'a t -> 'a t val import_goods: 'a t -> 'a t val
+   sell_goods: 'a t -> 'a t val popular_goods: 'a t -> 'a t val on_sale_goods:
+   'a t -> 'a t val in_stock_goods: 'a t -> 'a t val out_of_stock_goods: 'a t
+   end *)
