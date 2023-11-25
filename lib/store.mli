aa763415f3121a7471ca5391042d50269bca49bf
aeh245 <aeh245@cornell.edu>
2023-11-24 23:24:57 -0500

    store changes

--- lib/store.mli
+++ lib/store.mli
@@ -1,8 +1,15 @@
+open Bagofgoods
+open Items
+
 module type StoreType = sig 
-   type 'a t val count_products: 'a t ->
-   int val all_products: 'a t -> 'a t  val
-   sell_goods: int -> 'a -> 'a t -> 'a t 
-    val popular_goods: 'a t -> 'a t 
-   val in_stock_goods: 'a t -> 'a t val out_of_stock_goods: 'a t
-   val popular_goods: int -> 'a t -> 'a t
-   end 
+  type 'a t 
+  val count_products: 'a t -> int 
+  val all_products: 'a t -> 'a t  
+  val sell_goods: int -> 'a -> 'a t -> 'a t 
+  val popular_goods: int -> 'a t -> 'a t
+  val in_stock_goods: 'a t -> 'a t
+  val of_list: BagOfGoods.t list -> 'a t 
+  val to_list: 'a t -> BagOfGoods.t list
+end 
+
+module Store : StoreType 
